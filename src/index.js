dc7270cd67feaf37926be3332f051fd0952cde72
Julian Grinblat <julian@dotcore.co.il>
2020-04-29 18:23:08 +0900

    Add standard-as-callback

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import through from 'through2';
 import html2pug from 'html2pug';
 import vinylToString from 'vinyl-contents-tostring';
+import asCallback from 'standard-as-callback';
 import PluginError from 'plugin-error';
 
 const PLUGIN_NAME = 'gulp-html2pug';
@@ -16,11 +17,8 @@ const convert = (file) => (pug) => Object.assign(file, {
 
 // plugin level function (dealing with files)
 module.exports = (options = {}) => through.obj(function (file, enc, cb) {
-  vinylToString(file, enc)
+  asCallback(vinylToString(file, enc)
     .then((html) => html2pug(html, options))
     .then(convert(file.clone()))
-    .then((pugFile) => {
-      this.push(pugFile);
-      cb();
-    });
+    .then(this.push.bind(this)), cb);
 });
